ce58aa05956b647e06b329f923957f4d88a171f5
Ur1m <83430756+Ur1m@users.noreply.github.com>
2022-01-27 22:42:27 +0100

    Navbari
    bootstrap dhe rregullimi i navbarit

---
+++ front/src/Components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css';
+
+
+
+const Navbar = () => {
+    return(
+      
+        <nav class="navbar navbar-expand-lg navbar-light">
+             
+  <a class="navbar-brand" id='shkolla' href="#">ShkollaApp</a>
+ 
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" id='home' href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Exit</button>
+    </form>
+  </div>
+</nav>
+
+    )
+}
+export default Navbar;
