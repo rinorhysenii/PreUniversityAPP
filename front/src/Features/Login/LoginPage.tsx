b0cf69e3b0ef4f49fbc2c1f7ecf91328b0a7b1fc
Ur1m <83430756+Ur1m@users.noreply.github.com>
2022-01-28 19:05:10 +0100

    LoginPage i strukturuar

--- front/src/Features/Login/LoginPage.tsx
+++ front/src/Features/Login/LoginPage.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Button, Container, Header, Icon, Image, Input, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import '../../Features/Login/style.css';
 
 function LoginPage(){
 
     return(
-        <div><h1>thisis home</h1></div>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+        <Container className='aaad' text>
+            
+             <div className='kkk'>
+            <Header as='h1' inverted > 
+                <p data-text="School Management System"> School  Management  System</p>  
+            </Header>
+            </div>
+
+
+            <div className='logoja'>
+                <img src={require('./logo.png')} alt='s' style={{width:300,marginBottom:12}} className='logo' />
+            </div>
+                <>
+                    <Header as='h2'  className='asas' inverted content='Welcome to Hospital Management System' />
+
+                    <div className='forma'>
+                        <Input icon className='inputat' placeholder='Email'>
+                        <input />
+                        <Icon name='search' />
+                        </Input>
+                        <br />
+                        <br />
+                        <Input iconPosition='left' className='inputat' placeholder='Password'>
+                        <Icon name='at' />
+                        <input />
+                        </Input>
+                    </div>
+                </>
+            
+            
+        </Container>
+ </Segment>
     )
 }
 export default LoginPage;
