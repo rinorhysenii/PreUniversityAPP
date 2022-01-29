768061c82cfc89da92d834dd6b9bdef2f4c61fb5
berat422 <75537316+berat422@users.noreply.github.com>
2022-01-29 17:01:48 +0100

    ParentFunctions

---
+++ API/Controllers/ParentController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+   
+    public class ParentController : ControllerBase
+    {
+        public readonly IParentRepository _parentRepository;
+        public ParentController(IParentRepository parentRepository)
+        {
+            _parentRepository = parentRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> getMyChilds(Guid prid)
+        {
+            var resault =await _parentRepository.getMyChildren(prid);
+            return Ok(resault);
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> getTrascriptForMyChild(Guid id)
+        {
+           var res= _parentRepository.getTranskciptForMyChild(id);
+            return Ok(res);
+        }
+        [HttpGet("/average/id")]
+        public async Task<IActionResult> getavgForMyChild(Guid id)
+        {
+            var res = _parentRepository.getAvgForMyChild(id);
+            return Ok(res);
+        }
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,7 @@ namespace API
             });
             services.AddScoped<ITeacherRepository, TeacherRepository>();
             services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IParentRepository, ParentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
