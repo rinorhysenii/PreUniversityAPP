6a954ca19cb9bedd08013cda3f0638de61ae9a45
rinorhyseni <rinorhyseni18@gmail.com>
2022-01-29 17:26:58 +0100

    fix startup error

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,7 +46,6 @@ namespace API
             services.AddScoped<IParentRepository, ParentRepository>();
         }
 
-        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
