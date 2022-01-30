b84e0532ae194bf3b3005850605b0d63e23d4cb9
berat422 <75537316+berat422@users.noreply.github.com>
2022-01-30 07:23:57 +0100

    includs

--- Application/Repositories/StudentRepository.cs
+++ Application/Repositories/StudentRepository.cs
@@ -39,7 +39,7 @@ namespace Application.Repositories
 
         public async Task<List<Student>> getAllStudents()
         {
-            List<Student> students= await  _context.Students.ToListAsync();
+            List<Student> students= await  _context.Students.Include(x=> x.Parent).ToListAsync();
             if (students == null)
             {
                 throw new RestException(HttpStatusCode.BadRequest, "There are no courses!");
