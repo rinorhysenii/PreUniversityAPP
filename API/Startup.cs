ad5b160d945fbd76fc28f81697fcf2cdb8142e30
rinorhyseni <rinorhyseni18@gmail.com>
2022-01-31 21:37:39 +0100

    remove sql lite connection

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,10 +39,6 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsPolicy", policy =>
